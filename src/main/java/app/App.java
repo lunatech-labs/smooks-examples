acfba5d3bf83ce973d6e89929094fdc2756469af
Ludovico Fischer <ludovico.fischer@lunatech.com>
2011-11-08 17:05:05 +0100

    Inpt parsing seems to be working.

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,8 +1,11 @@
 package app;
 
 import org.milyn.Smooks;
+import org.milyn.payload.JavaResult;
 import org.xml.sax.SAXException;
 
+import javax.xml.transform.stream.StreamSource;
+import java.io.File;
 import java.io.IOException;
 import java.lang.String;
 
@@ -12,6 +15,7 @@ import java.lang.String;
 public class App {
       public static void main(String[] args) throws IOException, SAXException {
           Smooks smooks = new Smooks("smooks-config.xml");
-
+          JavaResult result = new JavaResult();
+          smooks.filterSource(new StreamSource(new File("fruit.txt")), result);
       }
 }

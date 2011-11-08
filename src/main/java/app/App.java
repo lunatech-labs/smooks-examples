553bca491171027b94759f8086091cbfbda89296
Ludovico Fischer <ludovico.fischer@lunatech.com>
2011-11-08 16:30:56 +0100

    Building properly.

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,15 +1,17 @@
 package app;
 
 import org.milyn.Smooks;
+import org.xml.sax.SAXException;
 
+import java.io.IOException;
 import java.lang.String;
 
 /**
  * @author Ludovico Fischer
  */
 public class App {
-      public static void main(String[] args) {
-          Smooks smooks = new Smooks();
+      public static void main(String[] args) throws IOException, SAXException {
+          Smooks smooks = new Smooks("smooks-config.xml");
 
       }
 }

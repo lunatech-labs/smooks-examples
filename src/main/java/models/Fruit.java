71898ff5bdb27a66b588f178a108bb6bbf28a1ae
Ludovico Fischer <ludovico.fischer@lunatech.com>
2011-11-08 16:42:41 +0100

    Added fruit example.

---
+++ src/main/java/models/Fruit.java
@@ -0,0 +1,25 @@
+package models;
+
+/**
+ * @author Ludovico Fischer
+ */
+public class Fruit {
+    private String fruit;
+    private int quantity;
+
+    public String getFruit() {
+        return fruit;
+    }
+
+    public void setFruit(String fruit) {
+        this.fruit = fruit;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}

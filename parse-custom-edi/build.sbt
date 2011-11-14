dcb3b7a8d2e09e6233a233d0ab025d7b093f92fe
Ludovico Fischer <ludovico.fischer@lunatech.com>
2011-11-14 14:39:09 +0100

    Parse EDIFACT and output a portion.

--- parse-custom-edi/build.sbt
+++ parse-custom-edi/build.sbt
@@ -4,6 +4,7 @@ version :=  "0.1-alpha"
 
 libraryDependencies ++= Seq(
                     "org.milyn" % "milyn-smooks-edi" % "1.5",
+                    "org.milyn" % "milyn-smooks-templating" % "1.5",
                     "org.milyn.edi.unedifact" % "d01a-mapping" % "1.5-SNAPSHOT"                
 )
 
--- parse-custom-edi/src/main/java/app/App.java
+++ parse-custom-edi/src/main/java/app/App.java
@@ -2,14 +2,15 @@ package app;
 
 import org.milyn.Smooks;
 import org.milyn.container.ExecutionContext;
+import org.milyn.event.report.HtmlReportGenerator;
 import org.milyn.io.StreamUtils;
+import org.milyn.payload.StringResult;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
+import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
-import java.io.ByteArrayInputStream;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 
 /**
  * @author Ludovico Fischer
@@ -18,8 +19,10 @@ public class App {
     public static void main(String[] args) throws IOException, SAXException {
         Smooks smooks = new Smooks("smooks-config.xml");
         try {
+            Writer writer = new StringWriter();
             ExecutionContext executionContext = smooks.createExecutionContext();
-            smooks.filterSource(executionContext, new StreamSource(new ByteArrayInputStream(StreamUtils.readStream(new FileInputStream("fruit.edi")))));
+            executionContext.setEventListener(new HtmlReportGenerator("target/reports/report.html"));
+            smooks.filterSource(executionContext, new StreamSource(new ByteArrayInputStream(StreamUtils.readStream(new FileInputStream("fruit.edi")))), new StringResult());
         } finally {
             smooks.close();
         }
